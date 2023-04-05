3e2c8119094931e703b06c585bf682267a4b7cee
guruprasath <guruprasath1051@gmail.com>
2023-04-06 00:17:17 +0530

    Calculating its Length

--- LineComparisionComputation/Program.cs
+++ LineComparisionComputation/Program.cs
@@ -10,7 +10,17 @@ namespace LineComparisionComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparision Computation");
+            //Define the two points
+            double x1 = 1;
+            double y1 = 2;
+            double x2 = 4;
+            double y2 = 6;
+
+            //Calculate the length of the line
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            //Print the length to the console
+            Console.WriteLine("The length of the line is :" +length);
             Console.ReadLine();
         }
     }
